552b584e740905b89aab98082038d348e9c4dbc5
khh <visualkhh@gmail.com>
2021-09-24 10:51:22 +0900

    example

--- example/src/users/UserDetaile.ts
+++ example/src/users/UserDetaile.ts
@@ -4,6 +4,5 @@ import {Sim} from "simple-boot-core/decorators/SimDecorator";
 @Sim()
 export class UserDetaile extends CustomModule{
     public print() {
-        console.log('UserDetaile--ppppppppp')
     }
 }
