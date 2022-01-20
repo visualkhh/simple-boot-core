42b4d554b0e694d4cbbf96ec1354a3c4c580a6d2
khh <khh@omnicns.com>
2022-01-20 14:20:27 +0900

    ssrfilter 속도개선  풀처리 AsyncBlockingQueue.ts

--- src/SimOption.ts
+++ src/SimOption.ts
@@ -1,6 +1,7 @@
 import {ConstructorType} from './types/Types';
 
 export class SimOption {
+    public name?: string;
     constructor(public advice: ConstructorType<any>[] = [], public proxy?: {onProxy: <T>(it: T) => T}) {
     }
 
